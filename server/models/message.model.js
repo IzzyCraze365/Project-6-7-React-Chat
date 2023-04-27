31f28be4cf5c91f0c1519580659d3e8358b230fe
IzzyCraze365 <johnaisabella3@gmail.com>
2023-04-27 13:58:16 -0400

    Completed Message & User Models

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -1,3 +1,33 @@
 // Project 6: React Chat
 // Team ALJI
 
+const mongoose = require("mongoose");
+const MessageSchema = new mongoose.Schema({
+  when: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+
+  user: {
+    type: String,
+    required: true,
+  },
+
+  room: {
+    type: String,
+    required: true,
+  },
+
+  body: {
+    type: String,
+    required: true,
+  },
+  user_id: {
+    // This is our foreign key to link User
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Message", MessageSchema); //! Never forget this
