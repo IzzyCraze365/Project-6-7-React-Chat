e0363c6f1315b1587bc3e9de265f7a0ffbf96e9d
IzzyCraze365 <johnaisabella3@gmail.com>
2023-04-27 17:59:51 -0400

    Added lots of comments to code, noticed that we forgot the Edit and Delete functions for the User

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,25 +1,25 @@
 // Project 6: React Chat
 // Team ALJI
 
+// Variable List
 const router = require("express").Router();
-const validateSession = require("../middleware/validate-session");
-console.log ("Test1");
-const Messages = require("../models/message.model");
-console.log ("Test2");
+const validateSession = require("../middleware/validate-session"); // Middleware to validate tokens
+const Messages = require("../models/message.model"); // Reference specific model
+
 // http://localhost:4000/message/create
 router.post("/create", validateSession, async (req, res) => {
   try {
-    const { when, user, room, body } = req.body;
-
+    const { when, user, room, body } = req.body; // paramters that need to be added when creating a Message
     const chatMessage = new Messages({
       when: when,
       user: user,
       room: room,
       body: body,
-      user_id: req.user._id,
+      user_id: req.user._id, // Auto-Generated
+      room_id: req.user.room._id, // Auto-Generated
     });
 
-    const newChatMessage = await chatMessage.save();
+    const newChatMessage = await chatMessage.save(); // Awaits the User Input
 
     res.status(200).json({
       message: "A new message has been added to the room",
@@ -33,10 +33,10 @@ router.post("/create", validateSession, async (req, res) => {
 // http://localhost:4000/message/display-all
 router.get("/display-all", validateSession, async (req, res) => {
   try {
-    let chatMessages = await Messages.find().populate("user_id", "name");
+    let chatMessages = await Messages.find().populate("room_id", "name"); // Displays Messages based on provided ID
 
     res.json({
-      message: "Messages currently posted in chat",
+      message: "Messages currently posted in chat Room",
       chatMessage: chatMessages,
     });
   } catch (error) {
@@ -46,11 +46,13 @@ router.get("/display-all", validateSession, async (req, res) => {
 
 // http://localhost:4000/message/delete/:id
 router.delete("/delete/:id", validateSession, async (req, res) => {
+  // Deletes Message based on specific Message ID
   try {
     const id = req.params.id;
     console.log("Message ID", id);
 
     const chatMessagesFound = await Messages.find({
+      // Confirms the person logged in is the person who posted the specific Message
       _id: req.params.id,
       user_id: req.user._id,
     });
@@ -59,6 +61,7 @@ router.delete("/delete/:id", validateSession, async (req, res) => {
     }
     console.log(req.user._id); //! TEST
     const removedChatMessage = await Messages.deleteOne({
+      // Only the message poster can delete it
       _id: id,
       user_id: req.user._id,
     });
@@ -78,8 +81,9 @@ router.delete("/delete/:id", validateSession, async (req, res) => {
 router.patch("/update/:id", validateSession, async (req, res) => {
   try {
     const { when, user, room, body } = req.body;
-    const filter = { _id: req.params.id, user_id: req.user._id };
+    const filter = { _id: req.params.id, user_id: req.user._id }; // Confirms the person logged in is the person who posted the specific Message ID
     const chatMessageToUpdate = {
+      // paramters that need to be added when creating a message
       when: when,
       user: user,
       room: room,
@@ -89,8 +93,8 @@ router.patch("/update/:id", validateSession, async (req, res) => {
     const returnUpdatedChatMessage = { new: true }; //default set to false
     const chatMessage = await Messages.findOneAndUpdate(
       filter,
-      chatMessageToUpdate,
-      returnUpdatedChatMessage
+      chatMessageToUpdate, // original Message that needs to be altered
+      returnUpdatedChatMessage // New version of the Message, Post change
     );
     if (!chatMessage) {
       throw Error("You are not able to edit this message");
