bc046785ed8320d83ccf470975d5076b3087a817
Ashlee Lindstrom <ashm0428@gmail.com>
2023-04-28 10:20:27 -0600

    changed isAdmin to be cleaner and better

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -10,23 +10,30 @@ const jwt = require("jsonwebtoken"); // Create the tokens that will be used in o
 // http:localhost:4000/user/create
 router.post("/create", async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body; // paramters that need to be signing up (creating a User)
+    const { firstName, lastName, email, password, isAdmin } = req.body; // parameters that need to be signing up (creating a User)
     const user = new User({
       firstName: firstName,
       lastName: lastName,
       email: email,
       password: bcrypt.hashSync(password, 10), // Auto-Encrypted
+      isAdmin: isAdmin,
     });
 
     const newUser = await user.save(); // Awaits Input
+    const adminToken = jwt.sign({ id: newUser._id }, process.env.JWT, {
+      expiresIn: 60 * 60 * 4, // Provides a token if the user is Admin. //! Token expires in 4 hr
+    });
 
-    let adminToken = jwt.sign({ id: newUser._id }, process.env.JWT, {
+    let token = jwt.sign({ id: user._id }, process.env.JWT, {
+      expiresIn: 60 * 60 * 4, // Provides a token if the passwords match.  //! Token expires in 4 hr
     });
 
     res.status(200).json({
       user: newUser,
-      message: "Created new React Chat user",
-      adminToken: adminToken,
+      message: user.isAdmin
+        ? "New Administrative Reach Chat User Created"
+        : "Created new React Chat user",
+      Token: user.isAdmin ? adminToken : token,
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -46,14 +53,27 @@ router.post("/login", async (req, res) => {
         user.password
       );
 
-      let token = jwt.sign({ id: user._id }, process.env.JWT, {
-        expiresIn: 60 * 60 * 12, // Provides a token if the passwords match.  //! Token expires in 12 hr
-      });
-
-      res.status(200).json({
-        message: passwordMatch ? "passwords matched" : "passwords do not match",
-        token: passwordMatch ? token : "invalid token",
-      });
+      if (user.isAdmin) {
+        let adminToken = jwt.sign({ id: newUser._id }, process.env.JWT, {
+          expiresIn: 60 * 60 * 4, // Provides a token if the user is Admin. //! Token expires in 4 hr
+        });
+        res.status(200).json({
+          message: passwordMatch
+            ? "passwords matched"
+            : "passwords do not match",
+          Token: passwordMatch ? adminToken : "invalid token",
+        });
+      } else {
+        let token = jwt.sign({ id: user._id }, process.env.JWT, {
+          expiresIn: 60 * 60 * 4, // Provides a token if the passwords match.  //! Token expires in 4 hr
+        });
+        res.status(200).json({
+          message: passwordMatch
+            ? "passwords matched"
+            : "passwords do not match",
+          token: passwordMatch ? token : "invalid token",
+        });
+      }
     } else {
       res.json({ message: "Email not found" });
     }
@@ -62,7 +82,6 @@ router.post("/login", async (req, res) => {
   }
 });
 
-
 // http://localhost:4000/user/delete/:id
 router.delete("/delete/:id", async (req, res) => {
   // Deletes User based on specific ID
@@ -70,11 +89,11 @@ router.delete("/delete/:id", async (req, res) => {
     const id = req.params.id;
 
     const userFound = await User.find({
-      _id: req.params.id,     
+      _id: req.params.id,
     });
     if (userFound.length === 0) {
       throw Error("404 User Not Found");
-    };
+    }
     const removedUser = await User.deleteOne({
       _id: id,
     });
@@ -90,12 +109,12 @@ router.delete("/delete/:id", async (req, res) => {
   }
 });
 
-// http://localhost:4000/user/update/:id 
+// http://localhost:4000/user/update/:id
 router.patch("/update/:id", async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body; // paramters that need to be updated by a User
-    const filter = { 
-      _id: req.params.id, 
+    const { firstName, lastName, email, password, isAdmin } = req.body; // paramters that need to be updated by a User
+    const filter = {
+      _id: req.params.id,
     }; // Confirms the person logged in can update the profile
     const userToUpdate = {
       // paramters that need to be changed when updating a User
@@ -103,6 +122,7 @@ router.patch("/update/:id", async (req, res) => {
       lastName: lastName,
       email: email,
       password: password,
+      isAdmin: isAdmin,
     };
 
     const returnUpdatedUser = { new: true };
