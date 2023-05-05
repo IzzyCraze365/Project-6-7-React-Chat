e00b0c2a8ecfced198cecefcf5a53e5f5ee2059e
Ashlee Lindstrom <ashm0428@gmail.com>
2023-05-05 11:34:57 -0600

    Began work on RoomIndex and RoomCreate

--- client/components/Rooms/RoomIndex.jsx
+++ client/components/Rooms/RoomIndex.jsx
@@ -1,3 +1,27 @@
 // Project 7: React Chat
 // Team ALJI
 
+import React, { useState, useEffect } from 'react';
+import { Col, Container, Row } from "reactstrap";
+// TODO: import RoomCreate and MessageIndex
+
+const RoomIndex = (props) => {
+
+    return ( 
+        <>
+        <Container>
+            <Row>
+                <Col md="4">
+                    {/* Room Create */}
+                    {/* User Edit */}
+                </Col>
+                <Col md="8">
+                    {/* Message Index */}
+                </Col>
+            </Row>
+        </Container>
+        </>
+     );
+}
+ 
+export default RoomIndex;
