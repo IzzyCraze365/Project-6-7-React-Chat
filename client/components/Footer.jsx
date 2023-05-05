488c0ca3a10fbc5a43d1da4e0a654dba22024a08
IzzyCraze365 <johnaisabella3@gmail.com>
2023-05-05 11:29:31 -0400

    Worked on Sign-up & Login

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,3 +1,16 @@
 // Project 7: React Chat
 // Team ALJI
 
+const Footer = (props) => {
+    return (
+      <footer className="container-fluid text-center p-3 mt-auto">
+        <h6>Upright Education</h6>
+        <h6>Software Development Bootcamp</h6>
+        <h6>Week 6 & 7 Project</h6>
+        <h5>React Chat</h5>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,3 +1,14 @@
 // Project 7: React Chat
 // Team ALJI
 
+const Header = (props) => {
+    return (
+      <header className="container-fluid text-center text-white p-5 mb-3 bg-info">
+        <h1>TEAM A.L.J.I.</h1>
+        <h2>React Chat</h2>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
