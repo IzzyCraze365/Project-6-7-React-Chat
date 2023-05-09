51c31f75d0aaf2d3b9139a748d8f1b055ba43552
Ashlee Lindstrom <ashm0428@gmail.com>
2023-05-09 12:29:56 -0600

    mounted UserEdit and MessageIndex to RoomIndex

--- client/react-chat/src/components/Rooms/Messages/MessageIndex.jsx
+++ client/react-chat/src/components/Rooms/Messages/MessageIndex.jsx
@@ -1,3 +1,12 @@
 // Project 7: React Chat
 // Team ALJI
 
+const MessageIndex = (props) => {
+    return ( 
+        <>
+        <h2>Hello from Message Index inside [Messages] inside [Rooms] </h2>
+        </>
+     );
+}
+ 
+export default MessageIndex;
---
+++ client/react-chat/src/components/Rooms/User/UserEdit.jsx
@@ -0,0 +1,13 @@
+// Project 7: React Chat
+// Team ALJI
+
+
+const UserEdit = (props) => {
+    return ( 
+        <>
+        <h2>Hello from UserEdit inside [User] inside [Rooms] </h2>
+        </>
+     );
+}
+ 
+export default UserEdit;
